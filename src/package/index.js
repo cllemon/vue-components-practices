f9f64136d61011b717e9539ac054b696ab17157c
lemon <19805815940@163.com>
2019-05-01 17:17:30 +0800

    refactor: New package component base-assembler.

---
+++ src/package/index.js
@@ -0,0 +1,15 @@
+import DynamicSelect from './dynamic-select/index.vue';
+import NoForm from './no-form/index.vue';
+import TablePanel from './table-panel/index.vue';
+import BaseAssembler from './base-assembler/index.vue';
+import PaginationPanel from './pagination-panel/index.vue';
+import FilterPanel from './filter-panel/index.vue';
+
+export default {
+  'no-form': NoForm,
+  'dynamic-select': DynamicSelect,
+  'table-panel': TablePanel,
+  'base-assembler': BaseAssembler,
+  'pagination-panel': PaginationPanel,
+  'filter-panel': FilterPanel,
+};
